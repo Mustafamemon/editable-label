b5d78589949bc5bc18d7553aa564a8243a7b8090
Mustafamemon <musatafamemon490@gmail.com>
2020-06-30 18:43:10 +0500

    editable label

---
+++ src/index.js
@@ -0,0 +1,143 @@
+import React, { useState, useEffect, useRef } from 'react';
+import PropTypes from 'prop-types';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faExternalLinkAlt, faPencilAlt } from '@fortawesome/free-solid-svg-icons';
+
+// import css from './css/editablelabel.css';
+
+const EditableLabel = props => {
+  const [view, setView] = useState('label');
+  const [value, setValue] = useState(props.initialValue);
+  const [previous, setPrevious] = useState(props.initialValue);
+  useEffect(() => {
+    if (view === 'text') {
+      textInput.current.focus();
+    }
+  }, [view, textInput]);
+  useEffect(() => {
+    setValue(props.initialValue || '-');
+  }, [props.initialValue]);
+  const textInput = useRef(null);
+  const keyUp = e => {
+    if (props.disableKeys === true) {
+      return;
+    }
+
+    if (e.key === 'Escape') {
+      setValue(previous || '-');
+      setView('label');
+    } else if (e.key === 'Enter') {
+      setValue(e.target.value || '-');
+      setPrevious(e.target.value || '-');
+      setView('label');
+
+      props.save(e.target.value);
+    }
+  };
+
+  const renderLabel = () => {
+    if (props.isWebsite)
+      return (
+        <a
+          href={'#'}
+          onClick={e => {
+            e.preventDefault();
+            setView('text');
+          }}
+        >
+          {value}
+        </a>
+      );
+    else
+      return (
+        <span
+          className={props.labelClass || ''}
+          onClick={() => {
+            setView('text');
+          }}
+        >
+          {value}
+        </span>
+      );
+  };
+
+  const renderInput = () => {
+    return (
+      <div>
+        <input
+          type={props.inputType}
+          value={value}
+          ref={textInput}
+          className={props.inputClass || ''}
+          onChange={e => {
+            setValue(e.target.value);
+          }}
+          onBlur={e => {
+            console.log(view)
+            setView('label');
+            setPrevious(e.target.value);
+            props.save(e.target.value);
+          }}
+          onKeyUp={keyUp}
+        />
+      </div>
+    );
+  };
+  return (
+    <div>
+      <h5>
+        {props.heading}
+        {props.isEditIcon?<span
+          className="edit-icon"
+          onClick={() => {
+            if(view === 'label'){ 
+              setView('text')
+            }
+            
+          }}
+        >
+          {<FontAwesomeIcon icon={faPencilAlt} size="1x" className="fs-10" />}
+        </span>:null}
+        {props.isWebsite === true ? (
+          <span className="web-icon">
+            <a href={{ value }}>
+              {
+                <FontAwesomeIcon
+                  icon={faExternalLinkAlt}
+                  size="1x"
+                  className="fs-10"
+                />
+              }
+            </a>
+          </span>
+        ) : null}
+
+        <div className={'pt-6'}>
+          <h6>{view === 'label' ? renderLabel() : renderInput()}</h6>
+        </div>
+      </h5>
+    </div>
+  );
+};
+
+export default EditableLabel;
+
+EditableLabel.propTypes = {
+  initialValue: PropTypes.string.isRequired,
+  save: PropTypes.func.isRequired,
+  labelClass: PropTypes.string,
+  inputClass: PropTypes.string,
+  inputType: PropTypes.string,
+  disableKeys: PropTypes.bool,
+  heading: PropTypes.string,
+  isWebsite: PropTypes.bool,
+  isEditIcon:PropTypes.bool,
+};
+EditableLabel.defaultProps = {
+  inputType: 'text',
+  disableKeys: false,
+  heading: '',
+  isWebsite: false,
+  isEditIcon:true,
+}
---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const webpack = require('webpack');
+const path = require('path');
+const WebpackNotifierPlugin = require('webpack-notifier');
+
+const BUILD_DIR = path.resolve(__dirname, 'build');
+const APP_DIR = path.resolve(__dirname, 'src');
+
+var config = {
+    mode: 'development',
+    devtool: 'source-map',
+    entry: [
+        APP_DIR + '/index.js'
+    ],
+    output: {
+        path: BUILD_DIR,
+        filename: 'index.js',
+        libraryTarget: 'umd'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js[x]?$/,
+                include: APP_DIR,
+                use: ['babel-loader']
+            },
+            {
+                test: /\.css$/i,
+                include: APP_DIR+'/css' ,
+                use: ['style-loader','css-loader']
+            }
+
+        ]
+    },
+    externals: {
+        "react": "react",
+        "react-dom": "react-dom",
+        "immutable": "immutable"
+    },
+    plugins: [
+        new WebpackNotifierPlugin({
+            alwaysNotify: true,
+            skipFirstNotification: true,
+        }),
+    ]
+};
+
+module.exports = config;
